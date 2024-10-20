38469907f97855d9226c24825d7a54e8f839ab40
timmcmic <timmcmic@microsoft.com>
2024-10-20 16:29:39 -0400

    Updating

--- EstablishGraphConnection.ps1
+++ EstablishGraphConnection.ps1
@@ -1,25 +1,15 @@
-$textBox2.Enabled = $false
-$textBox3.enabled = $false 
-}
-
-$TextBox3_TextChanged = {
-}
-$TextBox2_TextChanged = {
-}
-
-
-$RadioButton1_CheckedChanged = {
-    $textBox2.enabled = $true
-    $textBox3.enabled = $TRUE
-}
-
-$RadioButton2_CheckedChanged = {
-    $textBox2.Enabled = $false
-    $textBox3.enabled = $false 
-}
-
 function EstablishGraphConnection
 {
+    $RadioButton1_CheckedChanged = {
+        $textBox2.enabled = $true
+        $textBox3.enabled = $TRUE
+    }
+    
+    $RadioButton2_CheckedChanged = {
+        $textBox2.Enabled = $false
+        $textBox3.enabled = $false 
+    }
+
     Add-Type -AssemblyName System.Windows.Forms
     . (Join-Path $PSScriptRoot 'establishgraphconnection.designer.ps1')
     $Form1.ShowDialog()
