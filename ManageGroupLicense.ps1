d88aa6bd86204d3161f78c34b5caa5562d704849
timmcmic <timmcmic@microsoft.com>
2024-10-21 11:37:22 -0400

    Updating

--- ManageGroupLicense.ps1
+++ ManageGroupLicense.ps1
@@ -1,6 +1,5 @@
 function ManageGroupLicense
 {
-    Add-Type -AssemblyName System.Windows.Forms
-    . (Join-Path $PSScriptRoot 'establishgraphconnection.designer.ps1')
+    . (Join-Path $PSScriptRoot 'managegrouplicense.designer.ps1')
     $Form2.ShowDialog()
 }
