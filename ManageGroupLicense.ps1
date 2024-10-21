92ba16ea7c678f547be9047c7b716e4f447bc71f
timmcmic <timmcmic@microsoft.com>
2024-10-21 16:59:03 -0400

    Updating

--- ManageGroupLicense.ps1
+++ ManageGroupLicense.ps1
@@ -5,4 +5,7 @@ function ManageGroupLicense
         $groupID = $groupObjectIDText.Text
         write-host "GroupID"
     }
+
+    . (Join-Path $PSScriptRoot 'establishgraphconnection.designer.ps1')
+    $Form2.ShowDialog()
 }
