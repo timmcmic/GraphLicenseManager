64b3d8bac44ea01fae290c977065f38c87ca21c9
timmcmic <timmcmic@microsoft.com>
2024-10-22 10:08:27 -0400

    Updating

--- GraphLicenseManager.psm1
+++ GraphLicenseManager.psm1
@@ -1,7 +1,21 @@
 function Start-GraphLicenseManager
 {
-    $ErrorActionPreference = 'Stop'
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$logFolderPath
+    )
 
+    $ErrorActionPreference = 'Stop'
+    $global:logFile=$NULL
+    $logFileName = "LicenseChangeOperation_"+(Get-Date -Format FileDateTime)
+
+    new-logfile -logFileName $logFileName -logFolderPath $logFolderPath
+
+    out-logfile -string "************************************************************************"
+    out-logfile -string "Starting graph license manager."
+    out-logfile -string "************************************************************************"
+    
     establishGraphConnection
 
     manageGroupLicense
