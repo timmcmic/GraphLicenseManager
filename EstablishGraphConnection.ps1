79885c6f19973ced067b23a4e9692eebc2fb92da
timmcmic <timmcmic@microsoft.com>
2024-10-23 14:19:53 -0400

    Updating

--- EstablishGraphConnection.ps1
+++ EstablishGraphConnection.ps1
@@ -12,7 +12,7 @@ $Label1_Click = {
 function EstablishGraphConnection
 {
     $ExitButton_Click = {
-        void[]$form1.close()
+        [void]$form1.close
     }
     
     out-logfile -string "Entered establish graph connection..."
