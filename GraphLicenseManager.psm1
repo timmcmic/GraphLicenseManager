9225b54ab10e349b6511c263f85c85f1f4790242
timmcmic <timmcmic@microsoft.com>
2024-10-23 23:27:33 -0400

    Updating

--- GraphLicenseManager.psm1
+++ GraphLicenseManager.psm1
@@ -3,8 +3,35 @@ function Start-GraphLicenseManager
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
-        [string]$logFolderPath
+        [string]$logFolderPath,
+        [Parameter(Mandatory = $false)]
+        [boolean]$allowTelemetryCollection=$TRUE
     )
+
+    #Initialize telemetry collection.
+
+    $appInsightAPIKey = "63d673af-33f4-401c-931e-f0b64a218d89"
+    $traceModuleName = "GraphLicenseManager"
+
+    if ($allowTelemetryCollection -eq $TRUE)
+    {
+        start-telemetryConfiguration -allowTelemetryCollection $allowTelemetryCollection -appInsightAPIKey $appInsightAPIKey -traceModuleName $traceModuleName
+    }
+
+    #Define telemetry items.
+
+    $telemetryGraphLicenseManagerVersion = $NULL
+    $telemetryMSGraphAuthentication = $NULL
+    $telemetryMSGraphDirectory = $NULL
+    $telemetryMSGraphUsers = $NULL
+    $telemetryMSGraphGroups = $NULL
+    $telemetryOSVersion = (Get-CimInstance Win32_OperatingSystem).version
+    $telemetryStartTime = get-universalDateTime
+    $telemetryEndTime = $NULL
+    [double]$telemetryElapsedSeconds = 0
+    $telemetryEventName = "Start-DistributionListMigration"
+    [double]$telemetrySearches=0
+    [double]$telemetryCommits=0
 
     $ErrorActionPreference = 'Stop'
     $global:logFile=$NULL
@@ -13,6 +40,21 @@ function Start-GraphLicenseManager
     $global:exitSelected = $false
 
     new-logfile -logFileName $logFileName -logFolderPath $logFolderPath
+
+    out-logfile -string "********************************************************************************"
+    out-logfile -string "NOTICE"
+    out-logfile -string "Telemetry collection is now enabled by default."
+    out-logfile -string "For information regarding telemetry collection see https://timmcmic.wordpress.com/2022/11/14/4288/"
+    out-logfile -string "Administrators may opt out of telemetry collection by using -allowTelemetryCollection value FALSE"
+    out-logfile -string "Telemetry collection is appreciated as it allows further development and script enhancement."
+    out-logfile -string "********************************************************************************"
+
+    $telemetryGraphLicenseManagerVersion = Test-PowershellModule -powershellModuleName "GraphLicenseManager" -powershellVersionTest:$TRUE
+    $telemetryMSGraphAuthentication = Test-PowershellModule -powershellModuleName "Microsoft.Graph.Authentication" -powershellVersionTest:$TRUE
+    $telemetryMSGraphDirectory = Test-PowershellModule -powershellModuleName "Microsoft.Graph.DirectoryManagement" -powershellVersionTest:$TRUE
+    $telemetryMSGraphUsers = Test-PowershellModule -powershellModuleName "Microsoft.Graph.Users" -powershellVersionTest:$TRUE
+    $telemetryMSGraphGroups = Test-PowershellModule -powershellModuleName "Microsoft.Graph.Groups" -powershellVersionTest:$TRUE
+
 
     out-logfile -string "************************************************************************"
     out-logfile -string "Starting graph license manager."
