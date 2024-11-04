53f0982ab716c8877aace5e817d68a7d45177d84
Tim McMichael <timmcmic@microsoft.com>
2024-11-04 12:00:42 -0500

    Updating

--- DisplayGroupInfo.ps1
+++ DisplayGroupInfo.ps1
@@ -55,6 +55,8 @@ function DisplayGroupInfo
                 UserPrincipalName = $functionUPN
                 ObjectType = $functionObjectType
             }
+
+            out-logfile -string $functionObject
         }
     }
 }
