d9231fa82f1a22c036c3f94751b066a6bc1ec707
Tim McMichael <timmcmic@microsoft.com>
2024-11-04 11:53:58 -0500

    Updating

--- DisplayGroupInfo.ps1
+++ DisplayGroupInfo.ps1
@@ -1,5 +1,5 @@
 $CloseDisplay_Click = {
-    $GroupInfo.exit()
+    $GroupInfo.close()
 }
 
 function DisplayGroupInfo
@@ -7,4 +7,23 @@ function DisplayGroupInfo
     Add-Type -AssemblyName System.Windows.Forms
     . (Join-Path $PSScriptRoot 'displaygroupinfo.designer.ps1')
     $GroupInfo.ShowDialog()
+
+    out-logfile -string "Obtaining group membership..."
+
+    $operationSuccessful = $FALSE
+
+    try
+    {
+        $graphGroupMembers = Get-MgGroupMember -GroupId $global:graphGroup.id -errorAction Stop
+        $operationSuccessful = $TRUE
+    }
+    catch
+    {
+        $errorText = $_
+        out-logfile -string "Unable to obtain graph group membership..."
+        out-logfile -string $_
+        [System.Windows.Forms.MessageBox]::Show("Unable to obtain graph group membership..."+$errorText, 'Warning')
+    }
+
+    out-logfile -string $graphGroupMembers
 }
