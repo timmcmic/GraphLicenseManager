b9bd4548b325d6f7b2d02f7e19a28f0698f23053
Tim McMichael <timmcmic@microsoft.com>
2024-11-04 11:06:50 -0500

    Updating

--- DisplayGroupInfo.ps1
+++ DisplayGroupInfo.ps1
@@ -9,7 +9,7 @@ function DisplayGroupInfo
     $operationSuccessful=$false
 
     try {
-        $graphGroupMember = Get-MgGroupMember -GroupId $global:graphGroup.id -All -ErrorAction
+        $graphGroupMember = Get-MgGroupMember -GroupId $global:graphGroup.id -All -ErrorAction STOP 
         $operationSuccessful = $TRUE
     }
     catch {
@@ -17,6 +17,36 @@ function DisplayGroupInfo
         out-logfile -string ("Error to obtain graph group membership..."+$errorText)
         [System.Windows.Forms.MessageBox]::Show("Unable to obtain graph group membership.."+$errorText, 'Warning')
     }
+
+    $graphGroupMemberArray = @()
+
+    foreach ($member in $GraphGRoupMember)
+    {
+        if ($member.AdditionalProperties.'@odata.Type' -eq "#microsoft.graph.user")
+        {
+            $functionObjectType = "User"
+            $functionUPN = $member.AdditionalProperties.UserPrincipalName
+        }
+        elseif($member.AdditionalProperties.'@odata.Type' -eq "#microsoft.graph.group")
+        {
+            $functionObjectType = "Group"
+            $functionUPN = "N/A"
+        }
+        elseif ($member.AdditionalProperties.'@odata.context' -eq "https://graph.microsoft.com/v1.0/$metadata#contacts/$entity")
+        {
+            $functionObjectType = "Contact"
+            $functionUPN = "N/A"
+        }
+
+        $functionObject = New-Object PSObject -Property @{
+            ID = $member.Id
+            DisplayName = $member.additionalProperties.displayName
+            UserPrincipalName = $functionUPN
+            ObjectType = $functionObjectType
+        }
+    }
+
+    out-logfile -string "Obtaining all group members in an error status."
 
     if ($operationSuccessful -eq $TRUE)
     {
