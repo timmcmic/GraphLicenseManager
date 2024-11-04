370b083df9cc29a9fe9e76e246601d5b55fc41de
Tim McMichael <timmcmic@microsoft.com>
2024-11-04 07:04:49 -0500

    Updating

--- EstablishGraphConnection.ps1
+++ EstablishGraphConnection.ps1
@@ -194,7 +194,7 @@ Function EstablishGraphConnection
 
         out-logfile "Microsoft Graph Connection Information"
         out-logfile "--------------------------------------"
-        out-logfile " "
+        out-logfile ""
         out-logfile ("Connected to Tenant " + $Details.TenantId + " " +  $OrgName + " as account " + $Details.Account)
         out-logfile "+-------------------------------------------------------------------------------------------------------------------+"
         out-logfile ("The following permission scope is defined: " + $Scopes)
