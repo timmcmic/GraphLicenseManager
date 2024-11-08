01496633aa8f5eec7721fece9ad889b3bebdcd12
Tim McMichael <timmcmic@microsoft.com>
2024-11-08 10:21:54 -0500

    Updating

--- ManageGroupLicense.ps1
+++ ManageGroupLicense.ps1
@@ -398,38 +398,7 @@ function ManageGroupLicense
 
             $ToolLabel.Text = "Comparing SKU and SKU-Plan assignments on group to tenant SKU / SKU-Plans"
 
-            out-logfile -string "Evaluating the skus in the tenant against the group provided."
-
-            if ($global:graphGroupLicenses.assignedLicenses.count -gt 0)
-            {
-                out-logfile -string "The group specified has licenses - being the evaluation."
-
-                foreach ($skuObject in $global:skuTracking)
-                {
-                    out-logfile -string "Checking to see if the group has the SKU id..."
-
-                    if ($global:graphGroupLicenses.AssignedLicenses.SkuID.contains($skuObject.skuID))
-                    {
-                        out-logfile -string "The group licenses the sku id - check disabled plans..."
-
-                        $workingLicense = $global:graphGroupLicenses.assignedLicenses | where {$_.skuID -eq $skuObject.skuID}
-
-                        out-logfile -string ("Evaluating the following sku ID on the group: "+$workingLicense.skuID)
-
-                        if ($workingLicense.disabledPlans.contains($skuObject.ServicePlanID))
-                        {
-                            out-logfile -string "The plan is disabled - no work."
-                        }
-                        else
-                        {
-                            out-logfile -string "The sku is not disabled - set the SKU to enabled."
-                            $skuObject.EnabledOnGroup = $TRUE
-                        }
-                    }
-                }
-            }
-
-            out-xmlFile -itemToExport $global:skuTracking -itemNameToExport ("SkuTrackingGroupEvaluation-"+(Get-Date -Format FileDateTime))
+            EvaluateGroupLicenses
         }
 
         if ($getGroupFailure -eq $false)
