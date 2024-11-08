3d234e4ba74d1f62bef5bf65c4f265761ea49c75
Tim McMichael <timmcmic@microsoft.com>
2024-11-08 13:56:59 -0500

    Updating

--- CalculateSkuTracking.ps1
+++ CalculateSkuTracking.ps1
@@ -12,11 +12,29 @@ function CalculateSkuTracking
 
     foreach ($sku in $skus)
     {
+        if ($global:skuHash[$sku.skuPartNumber])
+        {
+            $skuCommonName = $global:skuHash[$sku.skuPartNumber].'???Product_Display_Name'
+        }
+        else 
+        {
+            $skuCommonName = $sku.SkuPartNumber    
+        }
+
         out-logfile -string ("Evaluating Sku: "+$sku.skuPartNumber)
 
         foreach ($servicePlan in $sku.ServicePlans)
         {
             out-logfile -string ("Evaluating Service Plan: "+$servicePlan.ServicePlanName)
+
+            if ($global:servicePlanHash[$servicePlan.servicePlanName])
+            {
+                $servicePlanCommonName = $global:servicePlanHash[$servicePlan.servicePlanName].Service_Plans_Included_Friendly_Names
+            }
+            else 
+            {
+                $servicePlanCommonName = $servicePlan.servicePlanName
+            }
 
             if ($servicePlan.AppliesTo -eq "User")
             {
@@ -25,9 +43,11 @@ function CalculateSkuTracking
                 $functionObject = New-Object PSObject -Property @{
                     SkuID = $sku.SkuId
                     SkuPartNumber = $sku.SkuPartNumber
+                    SkuCommonName = $skuCommonName
                     SkuPartNumber_ServicePlanName = $sku.SkuPartNumber+"_"+$servicePlan.ServicePlanName
                     ServicePlanID = $servicePlan.ServicePlanId
                     ServicePlanName = $servicePlan.ServicePlanName
+                    ServicePlanCommonName = $servicePlanCommonName
                     EnabledOnGroup = $false
                     EnabledNew = $false
                 }
