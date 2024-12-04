5a2e65b2425f035e1aa892cfc775c25379132e53
Timothy McMichael <timmcmic@microsoft.com>
2024-12-04 08:50:46 -0500

    Updating

--- CalculateError.ps1
+++ CalculateError.ps1
@@ -20,23 +20,57 @@ function CalculateError
 
         foreach ($entry in $result.Matches.Value)
         {
+            $tempString = ""
             out-logfile -string $entry
 
             if ($global:skuServicePlanIDHash[$entry])
             {
                 out-logfile -string $global:skuServicePlanIDHash[$entry].Service_Plans_Included_Friendly_Names
+                $tempString = $global:skuServicePlanIDHash[$entry].Service_Plans_Included_Friendly_Names
             }
             elseif ($global:skuGuidHash[$entry])
             {
                 out-logfile -string $global:skuGuidHash[$entry].'???Product_Display_Name'
+                $tempString = $global:skuGuidHash[$entry].'???Product_Display_Name'
+            }
+
+            if ($tempString -ne "")
+            {
+                out-logfile -string "A friendly name was located."
+
+                $errorString = $entry + " (" + $tempString + ")"
+
+                $errorText.replace($entry,$errorString)
             }
         }
     }
     else 
     {
+        $tempString = ""
+
         out-logfile -string "Only a single GUID patter was found in the error string."
 
         out-logfile -string $result.Matches.Value
+
+        if ($global:skuServicePlanIDHash[$result.Matches.Value])
+        {
+            out-logfile -string $global:skuServicePlanIDHash[$result.Matches.Value].Service_Plans_Included_Friendly_Names
+            $tempString = $global:skuServicePlanIDHash[$result.Matches.Value].Service_Plans_Included_Friendly_Names
+        }
+        elseif ($global:skuGuidHash[$result.Matches.Value])
+        {
+            out-logfile -string $global:skuGuidHash[$result.Matches.Value].'???Product_Display_Name'
+            $tempString = $global:skuGuidHash[$result.Matches.Value].'???Product_Display_Name'
+        }
+
+        if ($tempString -ne "")
+        {
+            out-logfile -string "A friendly name was located."
+
+            $errorString = $result.Matches.Value + " (" + $tempString + ")"
+
+            $errorText.replace($entry,$errorString)
+        }
     }
 
     out-logfile -string "Exiting Calculate Error Text"
