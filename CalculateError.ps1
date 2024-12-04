96dccabe23594741bc11ad34f6ab347f8d71c6dc
Timothy McMichael <timmcmic@microsoft.com>
2024-12-04 08:28:58 -0500

    Updating

--- CalculateError.ps1
+++ CalculateError.ps1
@@ -10,6 +10,15 @@ function CalculateError
 
     $errorText = ($errorText -split 'Status: 400')[0]
 
+    $guidPattern = '[({]?[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}[)}]?'
+    $result = $errorText | Select-String -Pattern $guidPattern -AllMatches
+    $result.Matches.Value
+
+    foreach ($item in $result)
+    {
+        out-logfile -string $item
+    }
+
     out-logfile -string "Exiting Calculate Error Text"
 
     return $errorText
